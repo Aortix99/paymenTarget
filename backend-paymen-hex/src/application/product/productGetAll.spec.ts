5eabc2f00ca231ba5e52f2af12d811f9465a7de5
ortiz99 <acortiz@softcaribbean.com>
2026-01-31 18:13:38 -0500

    agregando url para darle permisos de cors

--- backend-paymen-hex/src/application/product/productGetAll.spec.ts
+++ backend-paymen-hex/src/application/product/productGetAll.spec.ts
@@ -3,8 +3,12 @@ import { ProductRepository } from 'src/domain/product.repository';
 import { ProductGetAll } from './productGetAll';
 
 class FakeRepo implements ProductRepository {
-  constructor(private readonly data: Product[]) {}
+  constructor(private readonly data: Product[]) { }
   async getAll(): Promise<Product[]> {
+    return this.data;
+  }
+  async decrementStock(id: number, quantity: number): Promise<void> {}
+  async findOne(): Promise<Product[]> {
     return this.data;
   }
 }
@@ -13,8 +17,8 @@ describe('ProductGetAll', () => {
   it('returns all products from repository', async () => {
     const now = new Date();
     const products = [
-      new Product(1, 'A', 'Desc A', 10.5, 5, now),
-      new Product(2, 'B', 'Desc B', 20, 0, now),
+      new Product(1, 'A', 'Desc A', 'img', 10.5, 5, now),
+      new Product(2, 'B', 'Desc B', 'img', 20, 0, now),
     ];
     const repo = new FakeRepo(products);
     const useCase = new ProductGetAll(repo);
