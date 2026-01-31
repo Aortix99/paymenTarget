85a5e6265c1017f4ab8de7fd3206c5267d859c7d
ortiz99 <acortiz@softcaribbean.com>
2026-01-31 18:13:50 -0500

    agregando url para darle permisos de cors

--- backend-paymen-hex/src/main.ts
+++ backend-paymen-hex/src/main.ts
@@ -4,7 +4,7 @@ import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://paycard0202.s3-website.us-east-2.amazonaws.com/'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://paycard0202.s3-website.us-east-2.amazonaws.com'],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
